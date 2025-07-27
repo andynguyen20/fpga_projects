495a01460dc21a2b2c769706bad1a4fd9bb5ba27
andynguyen20 <andynguyenn20@gmail.com>
2025-07-26 21:51:03 -0400

    Reconfiguring files to display on github

--- project_1a/project_1a.srcs/sources_1/new/led_sw.v
+++ project_1a/project_1a.srcs/sources_1/new/led_sw.v
@@ -21,38 +21,10 @@
 
 
 module led_sw(
-    output [7:0] led, 
-    input [7:0] sw,
-    input btn0,
-    input btn1,
-    input btn2,
-    input btn3,
-    output anode0,
-    output anode1,
-    output anode2,
-    output anode3
-    );
-    
-    wire w1;
-    
-    assign anode0 = ~btn0;
-    assign anode1 = ~btn1;
-    assign anode2 = ~btn2;
-    assign anode3 = ~btn3;
-    
-    assign w1 = btn0 | btn1 | btn2 | btn3;
-    
-    assign led[0] = ~(sw[0] & w1);
-    assign led[1] = ~(sw[1] & w1);
-    assign led[2] = ~(sw[2] & w1);
-    assign led[3] = ~(sw[3] & w1);
-    assign led[4] = ~(sw[4] & w1);
-    assign led[5] = ~(sw[5] & w1);
-    assign led[6] = ~(sw[6] & w1);
-    assign led[7] = ~(sw[7] & w1);
-    
-    
-    
-   
-    
+    output led,
+    input sw
+);
+
+assign led = sw;
+
 endmodule
